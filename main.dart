420cd036e9ebd0eb32fc46981cee94961f193e6e
timdevir07 <rajukumarsdr95@gmail.com>
2024-08-11 19:25:16 +0530

    new comment

---
+++ main.dart
@@ -0,0 +1,13 @@
+void main() {
+  print('Hello Dart');
+  //single line comment
+  /*
+  this is multiline comment
+  2nd line Comment
+  */
+  var fName = 'Raju';
+  var lName = 'Kumar';
+  print('Hello $fName $lName');
+  print(fName.runtimeType);
+  print(lName.runtimeType);
+}
